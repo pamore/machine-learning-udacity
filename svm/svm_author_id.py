baa42629a9307e0e41641aedf5910f5a5a84eb67
Prachi Arun More <pamore@asu.edu>
2017-11-10 19:55:28 -0700

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,7 +24,29 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(kernel="rbf",C=10000)
+print "created SVM"
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+from sklearn.metrics import accuracy_score
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+print 'accuracy =',accuracy_score(pred, labels_test);
+num = len([i for i in pred if i==1])
+print 'Chris-',num
+
+#########################################################
+# OUTPUT-
+# created SVM
+# training time: 249.073 s
+# predicting time: 274.811 s
+# accuracy = 0.990898748578
+# Chris- 877
 #########################################################
 
 
