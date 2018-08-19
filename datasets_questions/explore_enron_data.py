766ce9694cde895c41dd983f9d891541ad6d87e9
pamore <pamore@asu.edu>
2018-08-19 16:14:12 -0400

    Data Set expored

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,32 @@
 """
 
 import pickle
-
+count = 0
+count2 = 0
+count3 = 0
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+for key in enron_data:
+    if enron_data[key]["salary"]==1:
+        count+=1
+print enron_data["SKILLING JEFFREY K"]
+for key in enron_data:
+    if enron_data[key]["total_payments"] == "NaN" and enron_data[key]["poi"]:
+        count+=1
+    if enron_data[key]["total_payments"] == "NaN":
+        count2+=1
+    count3+=1
 
-
+    # if enron_data[key]["salary"]!="NaN":
+    #     count2+=1
+    # if enron_data[key]["email_address"]!="NaN":
+    #     count3+=1
+print "count=",count
+print "count2=",count2
+print "count3=",count3
+print "per=",(count2*100/count3)
+    # if key.startswith("LAY"):
+    #     print enron_data[key]["total_payments"]
+    # if key.startswith("SKILLING"):
+    #     print enron_data[key]["total_payments"]
+    # if key.startswith("FASTOW"):
+    #     print enron_data[key]["total_payments"]
